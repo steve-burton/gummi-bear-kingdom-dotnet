85024cb417c287b0811a02ae332f1ad71e8694f6
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-02-10 10:45:36 -0800

    add gummi bear context and product.cs models

---
+++ src/gummi-bear-kingdom/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace gummi_bear_kingdom.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Country { get; set; }
+        public string Image { get; set; }
+    }
+}
--- src/gummi-bear-kingdom/Startup.cs
+++ src/gummi-bear-kingdom/Startup.cs
@@ -7,14 +7,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using gummi_bear_kingdom.Models;
 
 namespace gummi_bear_kingdom
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddEntityFramework()
+                .AddDbContext<GummiBearDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
