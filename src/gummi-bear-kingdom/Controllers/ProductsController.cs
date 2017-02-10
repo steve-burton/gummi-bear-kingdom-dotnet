24127b7e26801a3d74c03ff88b7b7b6ccff74338
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-02-10 11:26:28 -0800

    add products controller

---
+++ src/gummi-bear-kingdom/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gummi_bear_kingdom.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gummi_bear_kingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummiBearDbContext db = new GummiBearDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
